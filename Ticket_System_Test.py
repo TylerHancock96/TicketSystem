51ebf280c122b2f949aa06759dacdb52b2725877
TylerHancock96 <129103476+TylerHancock96@users.noreply.github.com>
2024-11-15 07:30:48 -0500

    Updated file. Replace the current pytest
    Working on this from my PC (thought I updated the project through pycharm but apparently I have more to understand.

--- Ticket_System_Test.py
+++ Ticket_System_Test.py
@@ -9,35 +9,58 @@ root = Tk()
 root.title("HTS")
 # Set Geometry (widthxheight)
 root.geometry('350x200')
+
+# Add menu bar
+# new item in menu bar labeled as 'New'
+# adding more items in the menu bar
+menu = Menu(root)
+item = Menu(menu)
+item.add_command(label='New')
+menu.add_cascade(label='File', menu=item)
+root.config(menu=menu)
+
 # Adding a label to the root window
-lbl = Label(root, text = "Do you need IT help?")
+lbl = Label(root, text="Do you need IT help?")
 lbl.grid()
+
+
 # Function to display text when button is clicked
 def clicked():
-    res = "You wrote" + txt.get()
+    res = "You wrote " + txt.get()
     lbl.configure(text=res)
+
+
 # Define close button
-def clickedClose():
+def progclose():
     sys.exit()
+
 
 # Adding Entry Field
 txt = Entry(root, width=10)
 txt.grid(column=1, row=0)
+
+
 # All widgets will be here
+# Showing and hiding widgets
+def show_widget():
+    txt.pack()
+
+
+def hide_widget():
+    txt.pack_forget()
+    b1.configure(command=hide_widget)
+
 
 # Button to continue program
 # Button with red text
-btn = Button(root, text="Yes", fg="red", command=clicked)
+b1 = Button(root, text="Yes", fg="red", command=show_widget)
 # Button Grid
-btn.grid(column=2, row=0)
+b1.grid(column=2, row=0)
 
 # Button to close program
-CloseBtn = Button(root, text="No", fg="blue", command=clickedClose)
+CloseBtn = Button(root, text="No", fg="blue", command=progclose)
 # Button Grid
 CloseBtn.grid(column=3, row=0)
 
 # Execute Tkinter
 root.mainloop()
-
-if clicked():
-    txt.grid(column=1, row=0)
