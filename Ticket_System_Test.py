04e3ad1a50327af1f165fb0617a5c899f0e737f7
TylerHancock96 <129103476+TylerHancock96@users.noreply.github.com>
2024-12-04 13:55:22 -0500

    Changes made to begin with two buttons
    Changes made to begin with two buttons that will either close the program or populate a textbox that initiates phase two of the submission process.

--- Ticket_System_Test.py
+++ Ticket_System_Test.py
@@ -1,5 +1,6 @@
 # Import Module
 import sys
+import tkinter
 from tkinter import *
 
 # Create root window
@@ -8,7 +9,7 @@ root = Tk()
 # Root window title and dimensions
 root.title("HTS")
 # Set Geometry (widthxheight)
-root.geometry('350x200')
+root.geometry('900x900')
 
 # Add menu bar
 # new item in menu bar labeled as 'New'
@@ -25,9 +26,9 @@ lbl.grid()
 
 
 # Function to display text when button is clicked
-def clicked():
-    res = "You wrote " + txt.get()
-    lbl.configure(text=res)
+# def clicked():
+#    res = "You wrote " + txt.get()
+#   lbl.configure(text=res)
 
 
 # Define close button
@@ -35,20 +36,17 @@ def progclose():
     sys.exit()
 
 
-# Adding Entry Field
-txt = Entry(root, width=10)
-txt.grid(column=1, row=0)
-
-
 # All widgets will be here
-# Showing and hiding widgets
+
+# Showing and hiding initial widgets
 def show_widget():
-    txt.pack()
-
-
-def hide_widget():
-    txt.pack_forget()
-    b1.configure(command=hide_widget)
+    lbl.configure(text="Please describe your issue: ")
+    txt = tkinter.Text(root, height=20, width=40)
+    txt.grid(column=1, row=1)
+    b1.grid_remove()
+    CloseBtn.configure(text="Close", command=progclose)
+    CloseBtn.grid(column=3, row=5)
+    b1_cont.grid(column=1, row=4)
 
 
 # Button to continue program
@@ -61,6 +59,14 @@ b1.grid(column=2, row=0)
 CloseBtn = Button(root, text="No", fg="blue", command=progclose)
 # Button Grid
 CloseBtn.grid(column=3, row=0)
+
+
+# Block of code to show next phase of system
+def phase_two():
+    lbl.configure(text="Please upload any pictures/videos to better describe your issue if you have any")
+
+
+b1_cont = Button(root, text="Continue", command=phase_two)
 
 # Execute Tkinter
 root.mainloop()
