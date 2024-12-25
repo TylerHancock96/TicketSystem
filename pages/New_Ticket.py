ce565484b4bd0dc29389e49ae573df9341103a99
tyler <hancocktyl96@gmail.com>
2024-12-24 21:47:15 -0500

    Tested on Mac

---
+++ pages/New_Ticket.py
@@ -0,0 +1,8 @@
+# import module
+import streamlit as st
+
+# Setting page title and favicon for window tab
+st.set_page_config(page_title="HTS - User Home", page_icon="🤖")
+
+# Setting header with data pulled from the login form to create unique usernames in header
+st.title("Welcome User!")
